da71a010e23ba93ca21f479eae38e40121dc0e91
Keith Owino <designsolutions1629@gmail.com>
2023-09-04 15:18:31 +0300

    Add primary navigation bar

--- projects/web-awesome/scripts/main.js
+++ projects/web-awesome/scripts/main.js
@@ -4,6 +4,8 @@ const appName = "web awesome";
 
 document.title = appName.toUpperCase();
 
+const titleElement = document.querySelector(".js-app-name");
 const siteTileRefElement = document.querySelector(".js-site-title-ref");
 
+titleElement.innerHTML = appName;
 siteTileRefElement.innerHTML = appName;
