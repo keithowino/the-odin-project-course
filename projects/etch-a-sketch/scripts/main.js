f72bcfba1fbed57bc51db329814d2be0a1dcb59c
Keith Owino <designsolutions1629@gmail.com>
2023-09-11 14:25:51 +0300

    Build canvas functionality.

--- projects/etch-a-sketch/scripts/main.js
+++ projects/etch-a-sketch/scripts/main.js
@@ -13,14 +13,26 @@ siteTileRefElement.innerHTML = appName;
 /*
 
 TODO:   PSEUDOCODE
-    *:  Create a webpage with a 16x16 grid of square divs.
+    *:  Create a webpage with a ${size}x${size} grid of square divs.
 
 */ 
 
-const gridElement = document.querySelector('.grid-container');
+document.addEventListener('DOMContentLoaded', () => {
+    grid(3);
+});
 
-function makeRows(rows, columns){
-    gridElement.style.setProperty()
+function grid(size){
+    const canvas = document.getElementById('grid-body');
+
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    const divisions = size * size;
+
+    for(let i = 0; i < divisions; i++){
+        const division = document.createElement('div');
+        division.style.backgroundColor = 'red';
+        canvas.insertAdjacentElement('beforeend', division)
+    };
+
 };
-
-makeRows(16, 16);
